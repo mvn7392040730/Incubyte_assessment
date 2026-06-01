d68f0175943888d9a75475118aeaeaa294ae602c
mvn7392040730 <42075234+mvn7392040730@users.noreply.github.com>
2026-06-01 16:01:56 +0000

    Playwright fixtures and dynamic configuration from .env added

---
+++ conftest.py
@@ -0,0 +1,23 @@
+import pytest
+from playwright.sync_api import sync_playwright
+from utils.config import BASE_URL, HEADLESS
+
+@pytest.fixture(scope="session")
+def base_url():
+    return BASE_URL
+
+
+@pytest.fixture(scope="session")
+def browser():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=HEADLESS)
+        yield browser
+        browser.close()
+
+
+@pytest.fixture(scope="function")
+def page(browser):
+    context = browser.new_context()
+    page = context.new_page()
+    yield page
+    context.close()
